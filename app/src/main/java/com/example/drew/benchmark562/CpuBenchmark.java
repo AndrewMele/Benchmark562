599dc41673cea96231a6e460436efab4979945b3
Drew <drew.mele@gmail.com>
2018-04-20 17:38:46 -0400

    First commit.

---
+++ app/src/main/java/com/example/drew/benchmark562/CpuBenchmark.java
@@ -0,0 +1,29 @@
+package com.example.drew.benchmark562;
+
+public class CpuBenchmark
+{
+    public void TestLowOrderOps()
+    {
+        // Operations such as:
+    }
+
+    public void TestHighOrderOps()
+    {
+        // Operations such as:
+    }
+
+    public void TestCompressionPerf()
+    {
+
+    }
+
+    public void TestSortingPerf()
+    {
+
+    }
+
+    public void TestHashingPerf()
+    {
+
+    }
+}
---
+++ app/src/main/java/com/example/drew/benchmark562/MemoryBenchmark.java
@@ -0,0 +1,37 @@
+package com.example.drew.benchmark562;
+
+import android.app.ActivityManager;
+
+// NOTES:
+// Apps are each given a fixed amount of heap (generally RAM) space
+
+public class MemoryBenchmark
+{
+    public void TestPrimaryMem()
+    {
+        // Sequential read test
+        //ActivityManager.MemoryInfo memoryInfo = getAvailableMemory();as
+
+        // Sequential write test
+
+        // Random read test
+
+        // Random read test
+
+        // Zeroing and Formatting
+
+    }
+
+    public void TestSecondaryMem()
+    {
+        // Sequential read test
+
+        // Sequential write test
+
+        // Random read test
+
+        // Random read test
+
+        // Zeroing and Formatting
+    }
+}
